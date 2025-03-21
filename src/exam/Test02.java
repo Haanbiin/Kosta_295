0d135d94b27f09fdccb11573a681fb259dc02b9f
HanbinKim <hbkimion@gmail.com>
2025-03-21 14:33:13 +0900

    로컬에서 함 더 수정함

--- src/exam/Test02.java
+++ src/exam/Test02.java
@@ -7,8 +7,12 @@ public class Test02 {
 	
 	public void bb() {
 		System.out.println("FULL TEST 중");
+		
 		System.out.println("원격에서 수정함");
-			
+		
+		System.out.println("로컬에서 수장함");
+		
+		System.out.println("로컬에서 수정 함 더 함");
 	}
 
 
