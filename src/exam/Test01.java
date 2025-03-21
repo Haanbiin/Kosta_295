ed81acfeac51d1ff9dc731ee09a951b8ec627930
HanbinKim <hbkimion@gmail.com>
2025-03-21 14:43:56 +0900

    test01 시나리오04

--- src/exam/Test01.java
+++ src/exam/Test01.java
@@ -3,6 +3,7 @@ package exam;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("쑥떡먹고싶다.");
+		System.out.println("로컬에서 수정함. 시나리오04");
 	}
 
 	public static void test(
